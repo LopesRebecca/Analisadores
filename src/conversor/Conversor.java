701b4a641c1850ffbd1a72543f1da4c6e0a9277c
Davi Fernandes Alves da Silva <daviifernandes06@gmail.com>
2022-01-13 00:14:49 -0300

    mudanças no algoritmo

--- src/conversor/Conversor.java
+++ src/conversor/Conversor.java
@@ -12,6 +12,8 @@ public class Conversor {
 
 	private int clause = 0;
 	private String formula;
+	
+	@SuppressWarnings("unused")
 	private String formulaConvertida;
 
 	public Conversor() {}
@@ -47,132 +49,78 @@ public class Conversor {
 				auxiliar = auxiliar.replaceAll("[()]","");
 						
 				if(!(i == 0)) {
-					formula[i-1] += chageType(auxiliar);
+					formula[i] = chageType(auxiliar);
+					formula[i] = move(auxiliar);
+					formula[i] = doubleNegation(auxiliar);
+					formula[i-1] += distribute(auxiliar);
+
+				}else {
+					formula[i] = chageType(auxiliar);
 				}
 		}
 		formula[0] = formula[0].replace("--", "");
 		
 		System.out.print("\n\n"+formula[0]+"\n\n\n");
 	}
+	
 	private String chageType(String auxiliar) {
 
-
-		Pattern implica = Pattern.compile("[^[a-z][>][a-z]]");
-		Pattern disjuncao = Pattern.compile("[^[-][a-z][#][a-z]]");
-		Pattern conjucao = Pattern.compile("[^[-][a-z][&][a-z]]");
-		Pattern negacao = Pattern.compile("[^[-][-][a-z]]");
-		Pattern morgan = Pattern.compile("[^[a-z][#][a-z][&][a-z]]");
+		Pattern implica = Pattern.compile("(?=[a-z][>][a-z])?{1}");
+//		Pattern disjuncao = Pattern.compile("([-][a-z][#][a-z])");
+//		Pattern conjucao = Pattern.compile("([-][a-z][&][a-z])");
+//		Pattern negacao = Pattern.compile("([-][-][a-z])");
+//		Pattern morgan = Pattern.compile("([a-z][#][a-z][&][a-z])");
 
 		Matcher i = implica.matcher(auxiliar);
-		Matcher d = disjuncao.matcher(auxiliar);
-		Matcher c = conjucao.matcher(auxiliar);
-		Matcher n = negacao.matcher(auxiliar);
-		Matcher m = morgan.matcher(auxiliar);
+//		Matcher d = disjuncao.matcher(auxiliar);
+//		Matcher c = conjucao.matcher(auxiliar);
+//		Matcher n = negacao.matcher(auxiliar);
+//		Matcher m = morgan.matcher(auxiliar);
 
 		String a = auxiliar;
 		
 		if (i.lookingAt()) {
-			//(x > y)  = (-x # y)
-			a =  "(-" +  auxiliar.replaceAll("\\>", "#")+ ")";
-			//out.println(nova);
+			a =  	"(-" +  auxiliar.replaceAll("\\>", "#")+ ")";
+			int teste = i.groupCount();
+			int p = i.regionStart();
+			int l = i.regionEnd();
+			out.println(a);
 		}
-		else if(d.lookingAt()) {
-			//-x#y = (-x & -y)
-			a =  "(-" +  auxiliar.replaceAll("\\#", "& -") + ")";
-			out.println(d);
-		}
-		else if (c.lookingAt()) {
-			//-x&y = (-x # -y)
-			a =  "(" +  auxiliar.replaceAll("\\&", "#") + ")";
-		}
-		else if (n.lookingAt()) {
-			//--x  = x
-			//System.out.println(i.matches());
-			a =  "(" +  auxiliar.replaceAll("\\-\\-", " ") + ")";
-		}
-		else if (m.lookingAt()) {
-			//x # y & z
-			// x # y & x # y
-			a =  "(" +  auxiliar.replaceAll("\\&", " )&(x #")+ ")";
-			//String b = a + "(-" +  auxiliar.replaceAll("\\&", "#")+ ")";
-		}
+//		else if(d.lookingAt()) {
+//			//-x#y = (-x & -y)
+//			a =  "(-" +  auxiliar.replaceAll("\\#", "& -") + ")";
+//			out.println(d);
+//		}
+//		else if (c.lookingAt()) {
+//			//-x&y = (-x # -y)
+//			a =  "(" +  auxiliar.replaceAll("\\&", "#") + ")";
+//		}
+//		else if (n.lookingAt()) {
+//			//--x  = x
+//			//System.out.println(i.matches());
+//			a =  "(" +  auxiliar.replaceAll("\\-\\-", " ") + ")";
+//		}
+//		else if (m.lookingAt()) {
+//			//x # y & z
+//			// x # y & x # y
+//			a =  "(" +  auxiliar.replaceAll("\\&", " )&(x #")+ ")";
+//			//String b = a + "(-" +  auxiliar.replaceAll("\\&", "#")+ ")";
+//		}
 		return a;
 	}
-
-	/*
-	(x > y)     = (-x # y)
-	-(x # y)    = (-x & -y)
-	-(x & y)    = (-x # -y)
-	--x         = x
-	x # (y & z) = (x # y) & (x # y)
-	 */
-//		Stack<Character> stack = new Stack<Character>();
-//
-//		for (char element : formula.toCharArray()) {
-//			if (element == '(') {
-//				stack.push(element);
-//			} else {
-//				if (element == ')') {
-//					char close = (Character) stack.peek();
-//					if (element == ')' && close == '(') {
-//						stack.pop();
-//						this.clause++;
-//					}
-//				}
-//			}
-//		}
-//		String[] clauseForm = new String[clause];
-//		clauseForm = notNullString(clauseForm);
-//
-//		int indexClause = 0;
-//
-//		Stack<Character> stackClause = new Stack<Character>();
-//
-//		for (char element : formula.toCharArray()) {
-//			clauseForm[indexClause] += element;
-//
-//			if (element == '(') {
-//				stackClause.push(element);
-//			} else {
-//				if (element == ')') {
-//					char close = (Character) stackClause.peek();
-//					if (element == ')' && close == '(') {
-//						stackClause.pop();
-//						if (stackClause.isEmpty()) {
-//							indexClause++;
-//						}
-//					}
-//				}
-//			}
-//		}
-//
-//		while (isEmpty(clauseForm)) {
-//			for (int i = 0; i < clauseForm.length; i++) {
-//				if (haveClauses(clauseForm[i])) {
-//				} else {
-//					int startIndex = clauseForm[i].indexOf('(', clauseForm[i].indexOf('(') + 1);
-//					int endIndex = clauseForm[i].indexOf(')') + 1;
-//
-//					String removeClause = clauseForm[i].substring(startIndex, endIndex);
-//					clauseForm[i + 1] += clauseForm[i].substring(startIndex, endIndex);
-//					clauseForm[i] = clauseForm[i].replace(removeClause, "");
-//				}
-//			}
-//		}
-//		for (int i = 0; i < clauseForm.length; i++) {
-//			System.out.print(clauseForm[i]);
-//		}
-//
-//	public  String[] conversion(String[] list){
-//		for(int i = 0; i < list.length; i--){
-//			System.out.println("aaa");
-//			if(list[i] == ">"){
-//				System.out.println("não entra na condição");
-//			}
-//		}
-//		return list;
-//	}
-
+	
+	public String move(String auxiliar) {
+		return null;
+	}
+	
+	public String doubleNegation(String auxiliar) {
+		return null;
+	}
+	
+	public String distribute(String auxiliar) {
+		return null;
+	}
+	
 	public String[] notNullString(String[] list) {
 		for (int i = 0; i < list.length; i++) {
 			list[i] = "";
@@ -189,15 +137,6 @@ public class Conversor {
 		return false;
 	}
 
-	public boolean haveClauses(String clause) {
-		long count = clause.chars().filter(ocorrencias -> ocorrencias == '(').count();
-
-		if (count > 1) {
-			return false;
-		} else
-			return true;
-	}
-
 	public int getClause() {
 		return clause;
 	}
@@ -205,4 +144,14 @@ public class Conversor {
 	public void setClause(int clause) {
 		this.clause = clause;
 	}
+	
+	
+	
+	/*
+	(x > y)     = (-x # y)
+	-(x # y)    = (-x & -y)
+	-(x & y)    = (-x # -y)
+	--x         = x
+	x # (y & z) = (x # y) & (x # y)
+	 */
 }
